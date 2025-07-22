e4ae57402fad49e1f5b38e1ae211e3885dd3f2c5
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-07-22 23:18:44 +0600

    add exact solution of sum of good number

--- sum_of_good_number.js
+++ sum_of_good_number.js
@@ -1,14 +1,26 @@
 var sumOfGoodNumbers = function(nums, k) {
     let sum=0
-    for(let i=0;i<nums.length;i++){
-
-        if (i - k >= 0 && i + k < nums.length){
-        if((nums[i]>nums[i-k])&&(nums[i]>nums[i+k])){
-            sum=sum+nums[i]
-        }
-        }
-
+    for (let i = 0; i < nums.length; i++){
         
+        if (((i - k) < 0 || (nums[i - k] < nums[i])) && ((i + k) >= nums.length || (nums[i + k] < nums[i]))) {
+            sum+=nums[i]
+        }
+
+        // else if ((i + k) >= nums.length || (nums[i + k] < nums[i])) {
+        //     sum+=nums[i]
+        // }
+
+        // if (i - k >= 0 && i + k < nums.length){
+        // if((nums[i]>nums[i-k])&&(nums[i]>nums[i+k])){
+        //     sum=sum+nums[i]
+        // }
+        // }
+
+        // else if((i-k<0&&i+k<nums.length)|| (i-k>0 &&i+k>nums.length) ){
+        //     sum+=nums[i]
+        // }
     }
     return sum
 };
+
+console.log(sumOfGoodNumbers([1,3,2,1,5,4],2));
