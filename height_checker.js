2b84369bb52764e8a4443407bd2146ccc4354da1
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-07-26 18:49:32 +0600

    height checker

--- height_checker.js
+++ height_checker.js
@@ -1,12 +1,14 @@
 var heightChecker = function (heights) {
     
-    let newArr= [...heights]
+    let newArr= [...heights].sort((a,b)=>a-b)
     let counter=0
-    for (let i = 0; i < newArr.length; i++){
-        if (newArr[i] > newArr[i + 1]) {
-            [newArr[i], newArr[i + 1]] = [newArr[i + 1], newArr[i]]           
-        }
-    }
+    // for (let i = 0; i < newArr.length; i++){
+    //     if (newArr[i] > newArr[i + 1]) {
+    //         [newArr[i], newArr[i + 1]] = [newArr[i + 1], newArr[i]]           
+    //     }
+    // }
+    
+    
 
     for (let i = 0; i < heights.length; i++){
         if (newArr[i] !== heights[i]) {
