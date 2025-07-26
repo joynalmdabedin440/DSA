0b6f9317e7dd703fdeebb41d9b7ac4ca541d5ba0
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-07-26 18:47:39 +0600

    add height checker problem

---
+++ height_checker.js
@@ -0,0 +1,20 @@
+var heightChecker = function (heights) {
+    
+    let newArr= [...heights]
+    let counter=0
+    for (let i = 0; i < newArr.length; i++){
+        if (newArr[i] > newArr[i + 1]) {
+            [newArr[i], newArr[i + 1]] = [newArr[i + 1], newArr[i]]           
+        }
+    }
+
+    for (let i = 0; i < heights.length; i++){
+        if (newArr[i] !== heights[i]) {
+            counter++
+        }
+    }
+
+    return counter
+};
+
+console.log(heightChecker([5,1,2,3,4]));
