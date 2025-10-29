b6b124b8d90bd9e1969a2d4c887f1fd6749bb4c7
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-10-29 11:28:57 +0600

    add power of three problem using recursion

---
+++ power_of_three.js
@@ -0,0 +1,18 @@
+var isPowerOfThree = function(n) {
+    if (n < 0 || n == 0) {
+        return false
+    }
+    if (n === 1) {
+        return true
+    }
+
+
+    if (n % 3 === 0) {
+         return isPowerOfThree(n/3)
+    }
+
+   return false
+};
+
+
+console.log(isPowerOfThree(9)); // true
