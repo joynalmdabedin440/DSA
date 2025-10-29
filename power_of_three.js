9d2fbeec588d1fbb48d048aa8b0b91d996cae093
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-10-29 11:30:36 +0600

    add power of four problem using recursion

--- power_of_three.js
+++ power_of_three.js
@@ -14,5 +14,18 @@ var isPowerOfThree = function(n) {
    return false
 };
 
+//power of four
+var isPowerOfFour = function(n) {
+    if(n<0|| n===0)return false
+
+    if(n==1)return true
+
+    if(n%4==0){
+        return isPowerOfFour(n/4)
+    }
+
+    return false
+};
+
 
 console.log(isPowerOfThree(9)); // true
